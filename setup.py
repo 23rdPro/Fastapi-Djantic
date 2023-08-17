1958cf8ec4290b511f81933e1aaef0a4861ebea5
23rdPro <mailolumide@gmail.com>
2023-08-17 19:52:48 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    version='1.0.0',
+    description='Test Driven RESTful API Specification with FastApi and Djantic',
+    author='Olumide',
+    author_email='mailolumide@gmail.com',
+    name='REST',
+    packages=find_packages(),
+)
