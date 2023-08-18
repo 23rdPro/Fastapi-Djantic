e11f8510f3e50db898bffb022324c82cad337c7e
23rdPro <mailolumide@gmail.com>
2023-08-18 09:43:14 +0100

    added api views

---
+++ blog/blog_app/api/views.py
@@ -0,0 +1,23 @@
+# CRUD user object
+async def create_user():
+    pass
+
+
+async def get_users():
+    pass
+
+
+async def get_user():
+    pass
+
+
+async def me():
+    pass
+
+
+async def update_me():
+    pass
+
+
+async def delete_me():
+    pass
