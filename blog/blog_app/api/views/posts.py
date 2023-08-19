e9141c21bc428671d88235ade3344150bd96b4ba
23rdPro <mailolumide@gmail.com>
2023-08-20 00:38:14 +0100

    added post schema

---
+++ blog/blog_app/api/views/posts.py
@@ -0,0 +1,26 @@
+async def create_post():
+    pass
+
+
+async def get_posts():
+    pass
+
+
+async def get_post():
+    pass
+
+
+async def get_user_posts():
+    pass
+
+
+async def get_user_post():
+    pass
+
+
+async def update_post_me():
+    pass
+
+
+async def delete_post_me():
+    pass
