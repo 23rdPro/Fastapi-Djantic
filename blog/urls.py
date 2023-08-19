69c35f55c7389a55e7007de5c25d5f48c5f556c2
23rdPro <mailolumide@gmail.com>
2023-08-20 00:39:26 +0100

    added user schema

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from django.urls import path
 
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 from blog.blog_app.api.views import users, posts
 
 
 router = APIRouter()
+
+router.post(
+    "/users",
+    status_code=status.HTTP_201_CREATED,
+    summary="Create new oser",
+    tags=['users', ]
+)(users.create_user)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
