5e80977a7bd3eaa9828e0b8ee9a911dd37ef62a4
23rdPro <mailolumide@gmail.com>
2023-08-20 16:26:40 +0100

    added api dependencies

--- blog/urls.py
+++ blog/urls.py
@@ -7,12 +7,34 @@ from blog.blog_app.api.views import users, posts
 
 router = APIRouter()
 
+
 router.post(
     "/users",
     status_code=status.HTTP_201_CREATED,
-    summary="Create new oser",
+    summary="create new oser",
     tags=['users', ]
 )(users.create_user)
+
+router.post(
+    "/token",
+    status_code=status.HTTP_200_OK,
+    summary="authenticate user",
+    tags=['users', ]
+)(users.login)
+
+router.get(
+    "/users/me",
+    status_code=status.HTTP_200_OK,
+    tags=['users', ],
+    summary="get current user"
+)(users.me)
+
+router.get(
+    '/users',
+    status_code=status.HTTP_200_OK,
+    summary='get all users',
+    tags=['users', ]
+)(users.get_users)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
