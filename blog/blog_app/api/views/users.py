bf37ecade2fc45c6a6191c70eeeacffc2cba85de
23rdPro <mailolumide@gmail.com>
2023-08-20 16:25:46 +0100

    added api dependencies

--- blog/blog_app/api/views/users.py
+++ blog/blog_app/api/views/users.py
@@ -1,6 +1,7 @@
 import datetime
 
 from asgiref.sync import sync_to_async
+from blog.blog_app.api.dependencies import user_obj, active_user_obj
 from blog.blog_app.api.helpers import (
     create_user_sync,
     to_schema,
@@ -27,9 +28,12 @@ async def login(form: Annotated[OAuth2PasswordRequestForm, Depends()]):
         detail="Incorrect email or password",
         headers={"WWW-Authenticate": "Bearer"},
     )
-    if not await sync_to_async(authenticate)(form.username, form.password):
+    if not await sync_to_async(authenticate)(
+            username=form.username, password=form.password
+    ):
         raise http_exception
-    if not await User.objects.aget(username=form.username).is_active:
+    user = await User.objects.aget(username=form.username)
+    if not user.is_active:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail="Inactive user")
     token_expires = datetime.timedelta(weeks=1)
@@ -45,21 +49,29 @@ async def logout():
     pass
 
 
-async def get_users():
-    pass
-
-
 async def get_user():
     pass
 
 
-async def me():
-    pass
+async def get_users(user: Annotated[User, active_user_obj]):
+    users = User.objects.all()
+    await sync_to_async(print)(users)
+    return await sync_to_async(UserSchema.from_django)(users, many=True)
+
+
+async def me(user: Annotated[User, user_obj]):
+    return await to_schema(user, UserSchema)
 
 
 async def update_me():
     pass
 
 
-async def delete_me():
-    pass
+async def delete_me(user: Annotated[User, user_obj]):
+    yoozer = await sync_to_async(User.objects.filter)(id=user.pk)
+    if await yoozer.aexists():
+        await sync_to_async(yoozer.delete)()
+        return {'ok': True}
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="user not found")
