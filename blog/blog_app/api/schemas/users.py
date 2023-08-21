911a633ead2dadc40d3afb758b025be304087ee5
23rdPro <mailolumide@gmail.com>
2023-08-21 12:54:48 +0100

    added dependency for user api

--- blog/blog_app/api/schemas/users.py
+++ blog/blog_app/api/schemas/users.py
@@ -1,8 +1,13 @@
+# from __future__ import annotations
+
 from django.contrib.auth.models import User
 from djantic import ModelSchema
 
 
 class UserSchema(ModelSchema):
+    username: str or None = ''
+    password: str or None = ''
+
     class Config:
         model = User
         include = ['username', 'password']
